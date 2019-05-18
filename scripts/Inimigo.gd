dd3546a24cef878e23b737d4c2afbf910a103b55
Eduardo Costa <eduhcosta@live.com>
2019-05-18 13:33:11 -0300

    Define actions and colitions on character/anemy

---
+++ scripts/Inimigo.gd
@@ -0,0 +1,84 @@
+extends StaticBody2D
+
+# Variables
+var areaDetectavel = false
+var objetoPersonagem
+var direcaoInimigo = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# Character enter on areaInimigo
+	$areaInimigo.connect("body_entered", self, "_on_areaInimigo_body_entered")
+	
+	# Character leave on areaInimigo
+	$areaInimigo.connect("body_exited", self, "_on_areaInimigo_body_exited")
+	
+	# Start enimy animation
+	$AnimationPlayer.play("paradoEsquerda")
+	
+	# Connection animation player
+	$AnimationPlayer.connect("animation_finished", self, "endAnimation")
+	pass 
+
+func _on_areaInimigo_body_entered(body):
+	if (body.is_in_group("personagem")):
+		objetoPersonagem = body
+		body.objetoInimigo = self
+		areaDetectavel = true
+	pass
+	
+func _on_areaInimigo_body_exited(body):
+	if (body.is_in_group("personagem")):
+		objetoPersonagem = null
+		body.objetoInimigo = null
+		areaDetectavel = false
+	pass
+	
+func _physics_process(delta):
+	if (areaDetectavel):
+		meetCharacter()
+		if (objetoPersonagem != null):
+			identifySide(objetoPersonagem)
+	pass
+
+# Start correct animation on character side
+func meetCharacter():
+	if (
+		$AnimationPlayer.current_animation != "ataqueDireita" 
+		|| $AnimationPlayer.current_animation != "ataqueEsquerda" 
+	):
+		if ($Sprite.flip_h == false):
+			$AnimationPlayer.play("ataqueEsquerda")
+		else:
+			$AnimationPlayer.play("ataqueDireita")
+	pass
+
+# Intentify if character enter on enemy area
+func identifySide(body):
+	if (body.position.x < position.x):
+		changeSide(false)
+	else:
+		changeSide(true)
+	pass
+
+# Change enemy view direction
+func changeSide(dir):
+	if (dir):
+		direcaoInimigo = 1
+	else:
+		direcaoInimigo = -1
+	
+	# Change sprite side
+	$Sprite.flip_h = dir
+	pass
+
+
+# Finish animation
+func endAnimation(anim):
+	if anim == "ataqueDireita" || anim == "ataqueEsquerda":
+		if ($Sprite.flip_h == false):
+			$AnimationPlayer.play("paradoEsquerda")
+		else:
+			$AnimationPlayer.play("paradoDireita")
+	pass
+	
--- scripts/Personagem.gd
+++ scripts/Personagem.gd
@@ -1,20 +1,44 @@
 extends KinematicBody2D
 
+# Signals
+signal levarDano
+signal aumentarScore
+
 # Constants
 const GRAVITY = 300
 const UP = Vector2(0, -1)
 const SPEED = 100
 const JUMP = 180
+const CAMERAMAX = 0
+const CAMERAMIN = -50
 
 # Variables
 var motion = Vector2()
 var dir = 1 
+var cameraOffset = -50
+var hp = 100
+var colisorDano = false
+var objetoInimigo
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	# Conect damage area
+	$areaDano.connect("area_entered", self, "_on_areaDano_area_entered")
+	
+	# Connect to hit area
+	$areaBater.connect("area_entered", self, "_on_areaBater_area_entered")
+	
+	# Connect when exited to hit area
+	$areaBater.connect("area_exited", self, "_on_areaBater_area_exited")
+	
+	# Connect dead time
+	$tempoMorrer.connect("timeout", self, "tempoMorrer")
+		
+	# Start animation
+	$AnimationPlayer.play("parado")
+	pass 
 
-
+# Define behavior
 func _physics_process(delta):
 	# Gravidade
 	if !is_on_floor():
@@ -50,9 +74,117 @@ func _physics_process(delta):
 	if !is_on_floor():
 		if motion.y > 0:
 			if $AnimationPlayer.current_animation == "pulando":
-				# $AnimationPlayer.play("caindo")
-				pass
+				$AnimationPlayer.play("caindo")
+				
 			
 	# Physic
 	move_and_slide(motion, UP)
+	
+	# Move camera
+	$Camera2D.offset.y = cameraOffset
+	if Input.is_action_pressed("ui_down"):
+		cameraOffset += 1
+	if !Input.is_action_pressed("ui_down"):
+		cameraOffset -= 1
+		
+	# Max camera
+	if cameraOffset > 0 :
+		cameraOffset = CAMERAMAX
+		
+	# MIN camera
+	if cameraOffset < -50 :
+		cameraOffset = CAMERAMIN
+		
+	# Dead
+	if hp <= 0:
+		dead()	
+
 	pass
+
+# Dead method
+func dead():
+	$Particles2D.emitting = true
+	$Sprite.visible = false
+	$tempoMorrer.start()
+	set_physics_process(false)
+	pass
+
+# Restart scene
+func tempoMorrer():
+	get_tree().reload_current_scene()
+	pass
+
+# Colission with enemy
+func _on_areaDano_area_entered(area):
+	if area.is_in_group("inimigo"):
+		if $tempo_invencivel.time_left == 0:
+			if colisorDano == false:
+				$tempo_invencivel.start()
+				hit()
+				hp -= 10
+				emit_signal("levarDano", 10)
+			else:
+				area.get_owner().call_deferred("explodir")
+				motion.y = -(JUMP / 2)
+				$pulo.play()
+				emit_signal("aumentarScore", 50)
+	pass
+
+# Called when player is hit
+func hit():
+	# Color transition
+	$Tween.interpolate_property(
+		$Sprite, 
+		"modulate", 
+		Color(0.55, 0, 0, 1), 
+		Color(1, 1, 1, 1), 
+		.5, 
+		Tween.TRANS_BOUNCE, 
+		Tween.EASE_IN_OUT
+	)
+	# Size transition
+	$Tween.interpolate_property(
+		$Sprite, 
+		"scale", 
+		Vector2(0.5, 0.5), 
+		Vector2(1, 1), 
+		.5, 
+		Tween.TRANS_BOUNCE, 
+		Tween.EASE_IN_OUT
+	)
+	
+	# Enemy direction
+	var direction
+	if objetoInimigo != null:
+		if objetoInimigo.direcaoInimigo == -1:
+			direction = Vector2(position.x - 30, position.y)
+		else:
+			direction = Vector2(position.x + 30, position.y)
+				
+		$Tween.interpolate_property(
+			self,
+			"position",
+			Vector2(position.x, position.y),
+			direction,
+			.5, 
+			Tween.TRANS_BOUNCE, 
+			Tween.EASE_IN_OUT
+		)
+		
+		# Start tween
+		$dano.play()
+		$Tween.start()
+	pass
+	
+# Called when jump on head of enemy
+func _on_areaBater_area_entered(area):
+	if area.is_in_group("inimigo"):
+		colisorDano = true
+	pass
+	
+# Called when exited of enemy area	
+func _on_areaBater_area_exited(area):
+	if area.is_in_group("inimigo"):
+		colisorDano = false
+	pass
+	
